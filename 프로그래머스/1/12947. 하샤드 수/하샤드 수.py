52c37a734161aa8c02e1b605506cc39fecd63066
MunHK <116345058+MunHK@users.noreply.github.com>
2025-01-20 22:47:44 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,10 +1,2 @@
 def solution(x):
-    answer = 0
-    a=x
-    while(x>0):
-        answer+=x%10
-        x//=10
-    if a%answer==0:
-        return True
-    else:
-        return False
+    return x%sum(int(i) for i in str(x))==0
