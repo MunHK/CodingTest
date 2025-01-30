1e6dfd4a426929e9636d7f307cb72453f2806696
MunHK <116345058+MunHK@users.noreply.github.com>
2025-01-30 15:48:15 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,5 @@
 def solution(array, commands):
     b=[]
-    for i in commands:
-        a=sorted(array[i[0]-1:i[1]])
-        b.append(a[i[2]-1])
+    for i,j,k in commands:
+        b.append(sorted(array[i-1:j])[k-1])
     return b
