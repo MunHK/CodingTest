1902a8cdb332ac0d17363b774fc2be7f60e1f44c
MunHK <116345058+MunHK@users.noreply.github.com>
2025-02-12 16:22:27 +0900

    [Silver III] Title: 소수 구하기, Time: 640 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,12 @@
-max=1000000
-num=[i for i in range(max+1)]
-num[1]=0
-for i in range(2,int(max**0.5)+1):
-    if num!=0:
-        j=2
-        while i*j<=max:
-            num[i*j]=0
-            j+=1
-
 n,m = map(int,input().split())
-for i in range(n,m+1):
-    if num[i]:
-        print(num[i])
+
+result=[True]*(m+1)
+result[1]=False
+for i in range(2,int(m**0.5)+1):
+    if result:
+        for j in range(i*2,m+1,i):
+            result[j]=False
+
+for k in range(n,m+1):
+    if result[k]:
+        print(k)
