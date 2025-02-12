75c95d65969496b8eddab8dfc27dc63c07ff2ba1
MunHK <116345058+MunHK@users.noreply.github.com>
2025-02-12 16:24:27 +0900

    [Silver III] Title: 소수 구하기, Time: 304 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,9 +1,11 @@
 n,m = map(int,input().split())
 
 result=[True]*(m+1)
+
 result[1]=False
+
 for i in range(2,int(m**0.5)+1):
-    if result:
+    if result[i]:
         for j in range(i*2,m+1,i):
             result[j]=False
 
