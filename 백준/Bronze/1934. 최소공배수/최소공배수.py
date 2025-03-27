558d2837450bf82819c911958203f36153cc67f2
MunHK <116345058+MunHK@users.noreply.github.com>
2025-03-28 00:36:51 +0900

    [Bronze I] Title: 최소공배수, Time: 88 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,5 +1,6 @@
 import math
-n=int(input())
-for i in range(n):
-  a,b=map(int,input().split())
-  print(a*b//math.gcd(a,b))
+
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    print(a*b//math.gcd(a,b))
