5a3ba9a38beebe7252c3726d1a0dd6026bc33e26
MunHK <116345058+MunHK@users.noreply.github.com>
2025-04-01 15:16:15 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 168 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,7 @@
+n=int(input())
+pack=[0]+list(map(int,input().split()))
+num=pack
+for i in range(1,n+1):
+    for j in range(1,i+1): #i=4 j=1,2,3,4
+        num[i]=min(num[i],num[i-j]+pack[j])
+print(num[n])
