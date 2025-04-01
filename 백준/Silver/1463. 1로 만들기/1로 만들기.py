be4286e07e03ad5bc32ce7c71fd043d3960364a2
MunHK <116345058+MunHK@users.noreply.github.com>
2025-04-01 22:34:26 +0900

    Update 1로 만들기.py

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,9 +1,9 @@
-n=int(input())
-num=[0]*(n+1)
-for i in range(2,n+1):
-    num[i]=num[i-1]+1
-    if i%2==0:
-        num[i]=min(num[i],num[i//2]+1)
+x=int(input())
+n=[0]*(x+1)
+for i in range(2,x+1):
+    n[i]=n[i-1]+1
     if i%3==0:
-        num[i]=min(num[i],num[i//3]+1)
-print(num[n])
+        n[i]=min(n[i],n[i//3]+1)
+    if i%2==0:
+        n[i]=min(n[i],n[i//2]+1)
+print(n[x])
