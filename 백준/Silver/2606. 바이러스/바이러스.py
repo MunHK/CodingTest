44ed9bd9be9cfee25fc909b35cb113652dfba7bc
MunHK <116345058+MunHK@users.noreply.github.com>
2025-04-27 12:11:12 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,24 @@
-import sys
-input=sys.stdin.readline
-
 n=int(input())
 m=int(input())
-cnt=0
 
 graph=[[] for _ in range(n+1)]
 visited=[False]*(n+1)
+
 for _ in range(m):
     a,b=map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for i in range(1,n+1):
-    graph[i].sort()
-
-def dfs(graph,v,visited):
+def dfs(v):
+    global count
     visited[v]=True
+    
+    graph[v].sort()
     for i in graph[v]:
         if not visited[i]:
-            dfs(graph,i,visited)
-dfs(graph,1,visited)
-print(sum([1 for i in visited if i])-1)
+            count+=1
+            dfs(i)
+
+count=0
+dfs(1)
+print(count)
