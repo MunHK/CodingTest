3a7eb459f645dcb66547e24cf22d4c540b2b8b7a
MunHK <116345058+MunHK@users.noreply.github.com>
2025-05-15 13:01:56 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,12 @@
 n,m=map(int,input().split())
-arr=[0]*m
-visited=[False]*(n+1)
-result=[]
 
-def btk(k):
+arr=[0]*m
+
+def btk(start,k):
     if k==m:
-        if sorted(arr) not in result:
-            result.append(sorted(arr))
-            print(*arr)
+        print(*arr)
         return
-    for i in range(1,n+1):
-        if not visited[i]:
-            arr[k]=i
-            visited[i]=True
-            btk(k+1)
-            visited[i]=False
-btk(0)
+    for i in range(start,n+1):
+        arr[k]=i
+        btk(i+1,k+1)
+btk(1,0)
