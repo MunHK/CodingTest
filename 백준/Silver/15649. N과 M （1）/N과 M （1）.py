5bc78052585fcc1d96c369b5396b42cc16958b15
MunHK <116345058+MunHK@users.noreply.github.com>
2025-05-15 12:16:54 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,7 +1,16 @@
-import itertools
-n,k=map(int,input().split())
-num=[i for i in range(1,n+1)]
-for i in itertools.permutations(num,k):
-    for j in range(k):
-        print(i[j],end=' ')
-    print()
+n,m=map(int,input().split())
+
+arr=[0]*m
+visited=[False]*(n+1)
+
+def btk(k):
+    if k==m:
+        print(*arr)
+        return
+    for i in range(1,n+1):
+        if not visited[i]:
+            arr[k]=i
+            visited[i]=True
+            btk(k+1)
+            visited[i]=False
+btk(0)
