f06c8dde28c1f82a107ce54e82bcde29359cd542
MunHK <116345058+MunHK@users.noreply.github.com>
2025-05-18 16:37:56 +0900

    [Silver I] Title: 정수 삼각형, Time: 120 ms, Memory: 36504 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,18 @@
+n=int(input())
+tri=[]
+for _ in range(n):
+    tri.append(list(map(int,input().split())))
+if n==1:
+    print(tri[0][0])
+    exit(0)
+tri[1][0]+=tri[0][0]
+tri[1][1]+=tri[0][0]
+for i in range(2,n):
+    for j in range(i+1):
+        if j==0:
+            tri[i][j]+=tri[i-1][j]
+        elif j==i:
+            tri[i][j]+=tri[i-1][j-1]
+        else:
+            tri[i][j]+=max(tri[i-1][j-1],tri[i-1][j])
+print(max(tri[n-1]))
