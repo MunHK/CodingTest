6da0e13572c6f97eb41dbb2417af3fa75a95be85
MunHK <116345058+MunHK@users.noreply.github.com>
2025-07-12 10:12:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        int c=Integer.parseInt(st.nextToken());
+        if (a==b&&b==c&&a==c){
+            System.out.println(10000+a*1000);
+        }
+        else if(a!=b&&b!=c&&a!=c){
+            System.out.println(Math.max(Math.max(a,b),c)*100);
+        }
+        else{
+            if(a==b)
+                System.out.println(1000+a*100);
+            else if (b==c)
+                System.out.println(1000+b*100);
+            else if(a==c)
+                System.out.println(1000+a*100);
+        }
+    }
+}
