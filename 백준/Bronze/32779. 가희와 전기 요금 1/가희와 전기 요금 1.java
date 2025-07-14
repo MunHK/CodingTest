9e954f8077a813607d6bc4d2e23fc761e8183c8d
MunHK <116345058+MunHK@users.noreply.github.com>
2025-07-14 20:35:29 +0900

    [Bronze I] Title: 가희와 전기 요금 1, Time: 252 ms, Memory: 15720 KB -BaekjoonHub

---
+++ 백준/Bronze/32779. 가희와 전기 요금 1/가희와 전기 요금 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int m=Integer.parseInt(st.nextToken());
+
+            double result=(a*m*105.6)/(60*1000);
+
+            System.out.println((long)Math.floor(result));
+        }
+    }
+}
