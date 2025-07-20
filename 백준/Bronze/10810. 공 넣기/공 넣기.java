36331a05700df07e7f6ecb4e6dd3696d1bb3bed6
MunHK <116345058+MunHK@users.noreply.github.com>
2025-07-20 21:59:54 +0900

    [Bronze III] Title: 공 넣기, Time: 72 ms, Memory: 12012 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,26 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int n=Integer.parseInt(st.nextToken());
         int m=Integer.parseInt(st.nextToken());
-        int[] num=new int[n+1];
-
+        int[] basket = new int[n];
         for(int i=0;i<m;i++){
-            st=new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
-            int c=Integer.parseInt(st.nextToken());
-            for(int j=a;j<=b;j++)
-                num[j]=c;
+            StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st1.nextToken());
+            int b = Integer.parseInt(st1.nextToken());
+            int c = Integer.parseInt(st1.nextToken());
+            for(int j=a-1;j<=b-1;j++){
+                basket[j]=c;
+            }   
         }
-        StringBuilder sb=new StringBuilder();
-        for(int k=1;k<=n;k++)
-            sb.append(num[k]).append(" ");
-        System.out.println(sb);
+        for(int i=0;i<n;i++){
+            System.out.printf(basket[i]+" ");
+        }
     }
 }
