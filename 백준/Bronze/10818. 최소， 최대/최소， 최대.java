e09e325c09b551c61dfba914d0320b8e2328373f
MunHK <116345058+MunHK@users.noreply.github.com>
2025-07-20 21:23:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 872 ms, Memory: 128408 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,21 +1,15 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int n=Integer.parseInt(st.nextToken());
-        String[] word=br.readLine().split(" ");
-        int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            if(Integer.parseInt(word[i])<min){
-                min=Integer.parseInt(word[i]);
-            }
-            if(Integer.parseInt(word[i])>max){
-                max=Integer.parseInt(word[i]);
-            }
-        }
-        System.out.println(min+" "+max);
+        int n=Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int num[] = new int[n];
+        for(int i=0;i<n;i++)
+            num[i]=Integer.parseInt(st.nextToken());
+        
+        Arrays.sort(num);
+        System.out.println(num[0]+" "+num[n-1]);
     }
 }
