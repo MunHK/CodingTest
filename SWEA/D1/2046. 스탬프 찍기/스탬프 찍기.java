de0d620102b49e39901bd4fe618deafa3f30cf50
MunHK <116345058+MunHK@users.noreply.github.com>
2025-07-24 22:33:41 +0900

    [D1] Title: 스탬프 찍기, Time: 77 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            System.out.print("#");
+        }
+    }
+}
