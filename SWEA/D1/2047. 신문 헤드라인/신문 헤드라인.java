fcbef04c46a136f03eeb9eca6d4f715f0b1827d7
MunHK <116345058+MunHK@users.noreply.github.com>
2025-07-25 23:33:03 +0900

    [D1] Title: 신문 헤드라인, Time: 70 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in = br.readLine();
+        String result=in.toUpperCase();
+        System.out.println(result);
+    }
+}
