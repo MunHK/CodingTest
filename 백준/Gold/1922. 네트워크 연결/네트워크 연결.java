17d51eb35a672706203dd99ad5313e0ad9e13641
MunHK <116345058+MunHK@users.noreply.github.com>
2025-08-26 10:22:22 +0900

    [Gold IV] Title: 네트워크 연결, Time: 564 ms, Memory: 47684 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -2,49 +2,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-class Graph{
-	@Override
-	public String toString() {
-		return "Graph [n=" + n + ", m=" + m + ", d=" + d + "]";
-	}
+class Graph implements Comparable<Graph>{
 	int n,m,d;
 	Graph(int n,int m,int d){
 		this.n=n;
 		this.m=m;
 		this.d=d;
 	}
+	@Override
+	public int compareTo(Graph o) {
+		// TODO Auto-generated method stub
+		return this.d-o.d;
+	}
 }
 public class Main {
 	static int n,m;
 	static int tree[];
 	static int result=0;
+	static ArrayList<Graph> g;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n=Integer.parseInt(br.readLine());
 		m=Integer.parseInt(br.readLine());
-		Graph g[] = new Graph[m];
+		g=new ArrayList<>();
 		tree = new int[n];
 		for(int i=0;i<m;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
-			g[i]=new Graph(n-1,m-1,d);
+			g.add(new Graph(n-1,m-1,d));
 		}
 		
 		for(int i=0;i<n;i++) {
 			tree[i]=i;
 		}
 		
-		Arrays.sort(g,(a,b)->a.d-b.d);
+		Collections.sort(g);
 		
 		for(int i=0;i<m;i++) {
-			if(find(g[i].n)!=find(g[i].m)) {
-				union(g[i].n,g[i].m);
-				result+=g[i].d;
+			Graph gm = g.get(i);
+			if(find(gm.n)!=find(gm.m)) {
+				union(gm.n,gm.m);
+				result+=gm.d;
 			}
 		}
 		System.out.println(result);
@@ -56,12 +61,9 @@ public class Main {
 		return find(tree[i]);
 	}
 	static void union(int a,int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		
-		if(a>b) {
-			tree[aRoot]=bRoot;
-		}else
-			tree[bRoot]=aRoot;
+		a = find(a);
+		b = find(b);
+		if(a!=b)
+			tree[b]=a;
 	}
 }
