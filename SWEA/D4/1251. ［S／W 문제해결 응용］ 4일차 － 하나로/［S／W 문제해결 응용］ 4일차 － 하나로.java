3e468e56580e32cdc788330e6c5d26597cdf3dd0
MunHK <116345058+MunHK@users.noreply.github.com>
2025-08-27 13:29:33 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,144 ms, Memory: 109,492 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,82 +1,80 @@
-//20250826 과제
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Island implements Comparable<Island>{
-	int a,b;
-	double d;
+class Node implements Comparable<Node>{
+	int n;
+	double cost;
 
-	Island(int a,int b,double d) {
-		this.a=a;
-		this.b=b;
-		this.d=d;
+	Node(int n,double cost){
+		this.n=n;
+		this.cost=cost;
 	}
-
 	@Override
-	public int compareTo(Island o) {
+	public int compareTo(Node o) {
 		// TODO Auto-generated method stub
-		return Double.compare(this.d, o.d);
+		return Double.compare(this.cost, o.cost);
 	}
 	
 }
+
 public class Solution {
-	static int[][] graph;
-	static ArrayList<Island> is;
-	static int[] parent;
+	static int graph[][];
 	static int n;
-	public static void main(String[] args) throws Exception{
+	static double result,tex;
+	static List<Node> list[];
+	static boolean[] visited;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=t;tc++) {
+			
 			n=Integer.parseInt(br.readLine());
-			double tex,result=0;
-			int cnt=0;
+			list=new ArrayList[n];
+			visited=new boolean[n];
 			graph=new int[n][2];
-			parent=new int[n];
-			is=new ArrayList<>();
+			result=0;
+			
 			StringTokenizer stx = new StringTokenizer(br.readLine());
 			StringTokenizer sty = new StringTokenizer(br.readLine());
-			for(int i=0;i<n;i++) {
-				graph[i][0]=Integer.parseInt(stx.nextToken());
-				graph[i][1]=Integer.parseInt(sty.nextToken());
-				parent[i]=i;
-			}
 			tex=Double.parseDouble(br.readLine());
 			
 			for(int i=0;i<n;i++) {
+				graph[i][0]=Integer.parseInt(stx.nextToken());
+				graph[i][1]=Integer.parseInt(sty.nextToken());
+				list[i]=new ArrayList<>();
+			}
+			
+			for(int i=0;i<n;i++) {
 				for(int j=i+1;j<n;j++) {
-					double degree=Math.pow(Math.abs(graph[i][0]-graph[j][0]),2)+Math.pow(Math.abs(graph[i][1]-graph[j][1]),2);
-					is.add(new Island(i,j,degree));
+					double d = Math.pow(graph[i][0]-graph[j][0], 2) + Math.pow(graph[i][1]-graph[j][1], 2);
+					list[i].add(new Node(j,d));
+					list[j].add(new Node(i,d));
 				}
 			}
 			
-			Collections.sort(is);
-			for(int i=0;i<is.size();i++) {
-				Island island = is.get(i);
-				if(find(island.a)!=find(island.b)) {
-					union(island.a,island.b);
-					result+=tex*island.d;
-					cnt+=1;
-				}
-				if(cnt==n-1)
-					break;
-			}
+			prim(1);
 			System.out.println("#"+tc+" "+Math.round(result));
 		}
 	}
-	static int find(int i) {
-		if(parent[i]==i)
-			return i;
-		return parent[i]=find(parent[i]);
-	}
-	static void union(int a,int b) {
-		a=find(a);
-		b=find(b);
-		if(a!=b)
-			parent[b]=a;
+	static void prim(int start) {
+		Queue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(start,0));
+		while(!pq.isEmpty()) {
+			Node node = pq.poll();
+			if(visited[node.n]) continue;
+			
+			visited[node.n]=true;
+			result+=node.cost*tex;
+			for(Node p:list[node.n]) {
+				if(!visited[p.n])
+					pq.offer(p);
+			}
+		}
 	}
 }
