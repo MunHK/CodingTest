38e11e361aaa5ad9b06866f19c7ce2b1ba5d08cd
MunHK <116345058+MunHK@users.noreply.github.com>
2025-08-27 13:44:14 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,146 ms, Memory: 208,836 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,69 +1,74 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Graph implements Comparable<Graph>{
-	int a,b,c;
-	Graph(int a,int b,int c){
-		this.a=a;
-		this.b=b;
-		this.c=c;
+class Vertex implements Comparable<Vertex>{
+	int v,cost;
+	Vertex(int v,int cost){
+		this.v = v;
+		this.cost = cost;
 	}
 	@Override
-	public int compareTo(Graph o) {
+	public int compareTo(Vertex o) {
 		// TODO Auto-generated method stub
-		return Integer.compare(this.c, o.c);
+		return Integer.compare(this.cost, o.cost);
 	}
+	
 }
 
 public class Solution {
+	static List<Vertex> list[];
+	static boolean[] visited;
 	static int v,e;
-	static int parent[];
-	static ArrayList<Graph> graph;
-	
-	public static void main(String[] args) throws Exception{
+	static long result;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=t;tc++) {
-			long result=0;
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			v=Integer.parseInt(st.nextToken());
-			e=Integer.parseInt(st.nextToken());
-			parent=new int[v+1];
-			for(int i=1;i<=v;i++)
-				parent[i]=i;
-			graph=new ArrayList<>();
+			v = Integer.parseInt(st.nextToken());
+			e = Integer.parseInt(st.nextToken());
+			
+			list=new ArrayList[v+1];
+			result=0;
+			visited=new boolean[v+1];
+			
+			for(int i=1;i<=v;i++) 
+				list[i]=new ArrayList<>();
+			
 			for(int i=0;i<e;i++) {
 				st=new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
-				graph.add(new Graph(a,b,c));
+				list[a].add(new Vertex(b,c));
+				list[b].add(new Vertex(a,c));
 			}
-			Collections.sort(graph);
+			prim(1);
 			
-			for(int i=0;i<e;i++) {
-				Graph g = graph.get(i);
-				if(find(g.a)!=find(g.b)) {
-					result+=g.c;
-					union(g.a,g.b);
-				}
-			}
 			System.out.println("#"+tc+" "+result);
 		}
+		
 	}
 	
-	static void union(int a,int b) {
-		a=find(a);
-		b=find(b);
-		if(a!=b)
-			parent[b]=a;
-	}
-	static int find(int i) {
-		if(parent[i]==i)
-			return i;
-		return parent[i]=find(parent[i]);
+	static void prim(int start) {
+		Queue<Vertex> pq = new PriorityQueue<>();
+		pq.offer(new Vertex(start,0));
+		while(!pq.isEmpty()) {
+			Vertex ver = pq.poll();
+			if(visited[ver.v]) continue;
+			
+			result+=ver.cost;
+			visited[ver.v]=true;
+			for(Vertex p : list[ver.v]) {
+				if(!visited[p.v])
+					pq.offer(p);
+			}
+		}
 	}
 }
