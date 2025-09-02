c840e7926a0c217f70baa3b21711864edca2f3f2
MunHK <116345058+MunHK@users.noreply.github.com>
2025-09-02 10:29:47 +0900

    [Gold IV] Title: 최단경로, Time: 880 ms, Memory: 118972 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,68 +1,67 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
-	int v,w;
-	Node(int v,int w){
-		this.v=v;
-		this.w=w;
-	}
-	@Override
-	public int compareTo(Node o) {
-		// TODO Auto-generated method stub
-		return Integer.compare(this.w, o.w);
-	}
-	
-}
 public class Main {
-	static List<Node> list[];
-	static int v,e;
-	static int[] result;
-	public static void main(String[] args) throws IOException {
+	static int v,e,start;
+	static List<Vertex> list[];
+	static int result[];
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		v=Integer.parseInt(st.nextToken());
 		e=Integer.parseInt(st.nextToken());
-		int start=Integer.parseInt(br.readLine());
+		start=Integer.parseInt(br.readLine());
 		
 		list=new ArrayList[v+1];
 		result=new int[v+1];
+		
 		for(int i=1;i<=v;i++) {
 			list[i]=new ArrayList<>();
 			result[i]=Integer.MAX_VALUE;
 		}
 		for(int i=0;i<e;i++) {
-			st = new StringTokenizer(br.readLine());
+			st=new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			list[a].add(new Node(b,c));
+			list[a].add(new Vertex(b,c));
 		}
-		bfs(start);
+		
+		dij();
 		for(int i=1;i<=v;i++) {
-			System.out.println(result[i]!=Integer.MAX_VALUE?result[i]:"INF");
+			System.out.println(result[i]==Integer.MAX_VALUE?"INF":result[i]);
 		}
 	}
-	static void bfs(int start) {
-		PriorityQueue<Node> q = new PriorityQueue<>();
-		q.add(new Node(start,0));
+	static void dij() {
+		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+		pq.add(new Vertex(start,0));
 		result[start]=0;
-		while(!q.isEmpty()) {
-			Node node = q.poll();
-			
-			for(Node p : list[node.v]) {// 2 2, 3 3
-				if(result[p.v]>result[node.v]+p.w) {
-					result[p.v]=result[node.v]+p.w;
-					q.add(new Node(p.v,result[p.v]));
+		while(!pq.isEmpty()) {
+			Vertex now = pq.poll();
+			for(Vertex next : list[now.v]) {
+				if(result[next.v]>result[now.v]+next.w) {
+					pq.add(new Vertex(next.v,result[now.v]+next.w));
+					result[next.v]=result[now.v]+next.w;
 				}
 			}
 		}
+	}
+}
+
+class Vertex implements Comparable<Vertex>{
+	int v,w;
+	Vertex(int v,int w){
+		this.v=v;
+		this.w=w;
+	}
+	@Override
+	public int compareTo(Vertex o) {
+		// TODO Auto-generated method stub
+		return Integer.compare(this.w, o.w);
 	}
 }
