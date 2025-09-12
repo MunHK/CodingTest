fa969c8c88db75994e92bd4d3b5dba2ac2d5b768
MunHK <116345058+MunHK@users.noreply.github.com>
2025-09-12 14:07:57 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,130 ms, Memory: 102,476 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,46 +1,46 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Solution {
-   public static void main(String[] args) throws IOException {
-      BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
-      StringBuilder sb = new StringBuilder();
-      StringTokenizer st;
-      int T = Integer.parseInt(in.readLine());
-      for(int tc=1;tc<=T;tc++){
-         st = new StringTokenizer(in.readLine());
-         int N = Integer.parseInt(st.nextToken());
-         int[][] adj = new int[N][N];
- 
-         for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-               adj[i][j] = Integer.parseInt(st.nextToken());
-            }
-         }
- 
-         int min = Integer.MAX_VALUE;
-         for(int i=0;i<N;i++){
-            int tmp = 0;
-            int[] visited = new int[N];
-            Queue<Integer> queue = new ArrayDeque<>();
-            queue.offer(i);
- 
-            while(!queue.isEmpty()){
-               int x = queue.poll();
-               for(int j=0;j<N;j++){
-                  if(i==j) continue;
-                  if(visited[j]!=0 || adj[x][j]==0) continue;
-                  visited[j] = visited[x]+1;
-                  tmp += visited[j];
-                  queue.offer(j);
-               }
-            }
- 
-            if(min>tmp) min = tmp;
-         }
- 
-         sb.append("#").append(tc).append(" ").append(min).append("\n");
-      }
-      System.out.print(sb);
-   }
+	static int n;
+	static int[][] dist;
+	static int INF=1000000000;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=t;tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n=Integer.parseInt(st.nextToken());
+			dist=new int[n][n];
+			int result=INF;
+			
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					dist[i][j]=Integer.parseInt(st.nextToken());
+					if(dist[i][j]==0 && i!=j)
+						dist[i][j]=INF;
+				}
+			}
+			
+			for (int k = 0; k < n; k++) {
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+						dist[i][j]=Math.min(dist[i][j], dist[i][k]+dist[k][j]);
+					}
+				}
+			}
+			
+			for(int i=0;i<n;i++) {
+				int sum=0;
+				for(int j=0;j<n;j++)
+					sum+=dist[i][j];
+				result=Math.min(result, sum);
+			}
+			System.out.println("#"+tc+" "+result);
+		}
+	}
 }
