ee8bc88ecfc57416a1b096c2a93a2ddc25ebb98d
MunHK <116345058+MunHK@users.noreply.github.com>
2025-09-14 23:28:15 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 147 ms, Memory: 34,152 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,28 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int graph[][];
-	static int visited[][];
-	static int dx[]= {-1,1,0,0};
-	static int dy[]= {0,0,-1,1};
 	static int n;
-	public static void main(String[] args) throws Exception{
+	static int[][] map;
+	static int[][] visited;
+	static int[] dx= {-1,1,0,0};
+	static int[] dy= {0,0,-1,1};
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int t = Integer.parseInt(br.readLine());
-		for(int tc=1;tc<=t;tc++) {
+		int t=Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=t; tc++) {
 			n=Integer.parseInt(br.readLine());
-			graph=new int[n][n];
+			map=new int[n][n];
 			visited=new int[n][n];
+			
 			for(int i=0;i<n;i++) {
-				String[] in = br.readLine().split("");
+				String[] in=br.readLine().split("");
 				Arrays.fill(visited[i], Integer.MAX_VALUE);
 				for(int j=0;j<n;j++) {
-					graph[i][j]=Integer.parseInt(in[j]);
+					map[i][j]=Integer.parseInt(in[j]);
 				}
 			}
 			bfs();
@@ -31,37 +32,36 @@ public class Solution {
 		}
 	}
 	static void bfs() {
-		PriorityQueue<Node> q = new PriorityQueue<>();
-		q.add(new Node(0,0,0));
+		PriorityQueue<Sol> pq = new PriorityQueue<>();
+		pq.add(new Sol(0,0,0));
 		visited[0][0]=0;
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			if(now.x==n-1 && now.y==n-1) {
+		while(!pq.isEmpty()) {
+			Sol sol=pq.poll();
+			if(sol.x==n-1 && sol.y==n-1)
 				break;
-			}
 			for(int i=0;i<4;i++) {
-				int nx=now.x+dx[i];
-				int ny=now.y+dy[i];
-				if(nx>=0 && nx<n && ny>=0 && ny<n && visited[nx][ny]>graph[nx][ny]+now.w) {
-					visited[nx][ny]=graph[nx][ny]+now.w;
-					q.add(new Node(nx,ny,visited[nx][ny]));
+				int nx=sol.x+dx[i];
+				int ny=sol.y+dy[i];
+				if(nx>=0 && nx<n && ny>=0 && ny<n && visited[nx][ny]>sol.cost+map[nx][ny]) {
+					visited[nx][ny]=sol.cost+map[nx][ny];
+					pq.add(new Sol(nx,ny,visited[nx][ny]));
 				}
+					
 			}
 		}
 	}
-}
-
-class Node implements Comparable<Node>{
-	int x,y,w;
-	Node(int x,int y,int w){
-		this.x=x;
-		this.y=y;
-		this.w=w;
+	static class Sol implements Comparable<Sol>{
+		int x,y,cost;
+		Sol(int x,int y,int cost){
+			this.x=x;
+			this.y=y;
+			this.cost=cost;
+		}
+		@Override
+		public int compareTo(Sol o) {
+			// TODO Auto-generated method stub
+			return Integer.compare(this.cost, o.cost);
+		}
+		
 	}
-	@Override
-	public int compareTo(Node o) {
-		// TODO Auto-generated method stub
-		return Integer.compare(this.w, o.w);
-	}
-	
 }
