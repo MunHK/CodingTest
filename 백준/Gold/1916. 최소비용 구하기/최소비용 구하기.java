1e90da1ac1fb1eed18009b8b17cb6a5aa920f68a
MunHK <116345058+MunHK@users.noreply.github.com>
2025-09-15 11:04:54 +0900

    [Gold V] Title: 최소비용 구하기, Time: 356 ms, Memory: 50352 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,71 +1,78 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static List<bus> list[];
-	static int visit[];
-	static int n,m;
-	static int start,end;
-	public static void main(String[] args) throws Exception{
+	static int n,m,start,end;
+	static List<Bus> list[];
+	static int[] visit;
+	static int result=0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		n=Integer.parseInt(br.readLine());
 		m=Integer.parseInt(br.readLine());
 		
 		list=new ArrayList[n+1];
-		visit=new int[n+1];
-		
-		for(int i=1;i<=n;i++) {
+		for(int i=0;i<=n;i++) {
 			list[i]=new ArrayList<>();
-			visit[i]=Integer.MAX_VALUE;
 		}
+		visit=new int[n+1];
+		Arrays.fill(visit, -1);
 		for(int i=0;i<m;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			list[a].add(new bus(b,c));
+			list[a].add(new Bus(b,c));
 		}
 		st=new StringTokenizer(br.readLine());
 		start=Integer.parseInt(st.nextToken());
 		end=Integer.parseInt(st.nextToken());
 		
 		dij();
-		
-		System.out.println(visit[end]);
+		System.out.println(result);
 	}
+	
 	static void dij() {
-		PriorityQueue<bus> pq = new PriorityQueue<>();
-		pq.add(new bus(start,0));
+		PriorityQueue<Bus> pq = new PriorityQueue<>();
+		pq.add(new Bus(start,0));
 		visit[start]=0;
 		while(!pq.isEmpty()) {
-			bus now = pq.poll();
-			if(now.v==end)
+			Bus now = pq.poll();
+			if(now.go==end) {
+				result=now.cost;
 				break;
-			for(bus next : list[now.v]) {
-				if(visit[next.v]>next.cost+visit[now.v]) {
-					visit[next.v]=next.cost+visit[now.v];
-					pq.add(new bus(next.v,visit[next.v]));
+			}
+			for(Bus next:list[now.go]) {
+				if(visit[next.go]==-1) {
+					visit[next.go]=visit[now.go]+next.cost;
+					pq.add(new Bus(next.go,visit[next.go]));
+				}
+				else if(visit[next.go]>visit[now.go]+next.cost) {
+					visit[next.go]=visit[now.go]+next.cost;
+					pq.add(new Bus(next.go,visit[next.go]));
 				}
 			}
 		}
 	}
-}
-
-class bus implements Comparable<bus>{
-	int v,cost;
-	bus(int v,int cost){
-		this.v=v;
-		this.cost=cost;
-	}
-	@Override
-	public int compareTo(bus o) {
-		// TODO Auto-generated method stub
-		return Integer.compare(this.cost, o.cost);
-	}
 	
+	static class Bus implements Comparable<Bus>{
+		int go,cost;
+		Bus(int go,int cost){
+			this.go=go;
+			this.cost=cost;
+		}
+		@Override
+		public int compareTo(Bus o) {
+			// TODO Auto-generated method stub
+			return Integer.compare(this.cost, o.cost);
+		}
+		
+	}
 }
