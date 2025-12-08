b0d94cdee80595eff12682cababa7032067209a9
MunHK <116345058+MunHK@users.noreply.github.com>
2025-12-08 18:34:24 +0900

    [Silver IV] Title: 차집합, Time: 2844 ms, Memory: 252368 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -2,13 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
 	static int a,b;
-	static int[] aa;
-	static int[] bb;
 	static PriorityQueue<Integer> result = new PriorityQueue<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,47 +19,20 @@ public class Main {
 		a=Integer.parseInt(st.nextToken());
 		b=Integer.parseInt(st.nextToken());
 		
-		aa=new int[a];
-		bb=new int[b];
+		TreeSet<Integer> aa = new TreeSet<>();
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<a;i++) {
-			aa[i]=Integer.parseInt(st.nextToken());
+			aa.add(Integer.parseInt(st.nextToken()));
 		}
 
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<b;i++) {
-			bb[i]=Integer.parseInt(st.nextToken());
+			aa.remove(Integer.parseInt(st.nextToken()));
 		}
-		Arrays.sort(bb);
-		for(int i=0;i<a;i++) {
-			binSearch(aa[i]);
-		}
-		int size=result.size();
-		System.out.println(size);
-		for(int i=0;i<size;i++) {
-			System.out.print(result.poll()+" ");
-		}
-	}
-	private static void binSearch(int num) {
-		int left=0;
-		int mid=b/2;
-		int right=b-1;
-		while(true) {
-			if(bb[mid]==num) {
-				break;
-			}else if(bb[mid]>num) {
-				right=mid;
-				mid=(right+left)/2;
-			}else if(bb[mid]<num) {
-				left=mid;
-				mid=(right+left)/2;
-			}
-			if(Math.abs(right-left)<=1) {
-				if(bb[left]!=num && bb[right]!=num)
-					result.add(num);
-				break;
-			}
+		System.out.println(aa.size());
+		for(int num:aa) {
+			System.out.print(num+" ");
 		}
 	}
 }
